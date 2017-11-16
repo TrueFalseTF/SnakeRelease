494fd88753cb1166a3620671817bd4e2f8073cdb
TrueFalseTF <dimon_mcensk@mail.ru>
2017-11-16 15:31:44 +0300

    Оформлен Game Ower

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -59,10 +59,31 @@ namespace Snake
                 else
                 {
                     snake.Move(1,1);
-                }
+                }                 
+            }
 
-                     
-            }            
-        }        
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Дмитрий Чёпоров", xOffset + 2, yOffset++);            
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
     }
+
 }
